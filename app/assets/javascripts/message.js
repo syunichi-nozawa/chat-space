620b345f2353ca1b68e3bce6c3f1db50015273ce
syunichi-nozawa <nozawashunichi@Syunichi-no-MacBook-Pro.local>
2019-06-08 14:16:22 +0900

    Edit message.js
    Ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,13 @@
+$('.js-form').on('submit', function(){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+});
